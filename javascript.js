7aed4b392c0c849e64d65faeb678d0226af5d1e4
Jack Roudebush <roudebushjack@gmail.com>
2023-03-14 22:21:01 -0400

    Finish if... else if statement (first draft)

--- javascript.js
+++ javascript.js
@@ -24,7 +24,27 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     const playerSelection = prompt("What are you throwing? ");
+    let playerSelectionLower = playerSelection.toLowerCase();
     const computerSelection = getComputerChoice();
+
+    if (playerSelectionLower === "rock" && computerSelection === "rock") {
+        console.log("It's a tie!");
+    } else if (playerSelectionLower === "rock" && computerSelection === "paper") {
+        console.log("You Lose! Paper beats Rock!");
+    } else if (playerSelectionLower === "rock" && computerSelection === "scissors") {
+        console.log("You Win! Rock beats Scissors!");
+    } else if (playerSelectionLower === "paper" && computerSelection === "rock") {
+        console.log("You Win! Paper covers Rock!");
+    } else if (playerSelectionLower === "paper" && computerSelection === "paper") {
+        console.log("It's a tie!");
+    } else if (playerSelectionLower === "paper" && computerSelection === "scissors") {
+        console.log("You Lose! Scissors cut Paper");
+    } else if (playerSelectionLower === "scissors" && computerSelection === "rock") {
+        console.log("You Lose! Rock crushes Scissors!");
+    } else if (playerSelectionLower === "scissors" && computerSelection === "paper") {
+        console.log("You Win! Scissors cut Paper!");
+    } else if (playerSelectionLower === "scissors" && computerSelection === "scissors")
+    console.log("It's a tie!");
 }
 
 // Create a function called game()
